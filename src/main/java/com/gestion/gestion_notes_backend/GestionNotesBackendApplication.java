a272fff8c19af6023ceb68775f9abb41616197c7
Mahamat <mht.kaila17@gmail.com>
2026-05-30 22:08:56 +0200

    initial commit : structure Spring Boot

---
+++ src/main/java/com/gestion/gestion_notes_backend/GestionNotesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.gestion_notes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionNotesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionNotesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/gestion_notes_backend/GestionNotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.gestion_notes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionNotesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
